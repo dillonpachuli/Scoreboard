05e3df704a8a46efd3177e81b77b67b01ac713f8
Dillon Chapuli <148895731+dillonpachuli@users.noreply.github.com>
2024-10-07 16:35:30 +0000

    10/7/24

---
+++ Scoreboard.java
@@ -0,0 +1,29 @@
+public class Scoreboard
+{
+    private String teamOne;
+    private String teamTwo; 
+    private int teamOneScore;
+    private int teamTwoScore;
+    private String activeTeam;
+
+    public Scoreboard(String one, String two)
+    {
+        teamOne = one;
+        teamTwo = two;
+        activeTeam = teamOne;
+    }
+    
+    public void recordPlay(int score)
+    {
+        if(teamOne.equals(activeTeam))
+        {
+            
+        }
+    }
+
+    public String getScore()
+    {
+        return teamOneScore + "-" + teamTwoScore + "-" + activeTeam;
+    }
+
+}
